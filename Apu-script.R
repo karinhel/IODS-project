43812b09f8d752df81c803bc421bbaebd7eec0ae
karinhel <karin.a.helander@helsinki.fi>
2017-02-03 16:50:50 +0200

    wihii its friday

--- Apu-script.R
+++ Apu-script.R
@@ -9,3 +9,8 @@ str(learning2014)
 
 #Ei knittaa --> asennetaan rprojroot
 devtools::install_github("krlmlr/rprojroot")
+
+
+
+par(mfrow = c(2,2))
+plot(the_model, which=c(1, 2, 5), col=("violetred") )
